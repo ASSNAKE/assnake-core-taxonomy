b547a1cd767d4cc014f2fc4dbfa126e617c6eca8
Fedorov113 <fedorov.de@gmail.com>
2020-02-21 05:42:44 +0300

    Update snake_module_setup.py

--- assnake_core_taxonomy/snake_module_setup.py
+++ assnake_core_taxonomy/snake_module_setup.py
@@ -9,7 +9,7 @@ from assnake.utils import read_yaml
 this_dir = os.path.dirname(os.path.abspath(__file__))
 snake_module = SnakeModule(name = 'assnake-core-taxonomy', 
                            install_dir = this_dir,
-                           snakefiles = ['./cat_bat/cat_bat.py'],
+                           snakefiles = ['./cat_bat/cat_bat.py', './metaphlan2/metaphlan2.py'],
                            invocation_commands = [mp2_invocation],
                            initialization_commands = [mp2_init],
                            wc_configs = [read_yaml(os.path.join(this_dir, './metaphlan2/wc_config.yaml'))])
