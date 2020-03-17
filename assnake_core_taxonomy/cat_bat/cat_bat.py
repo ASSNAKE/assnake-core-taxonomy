98f19c672d7e1d265bd5aaa37bd8d92a5c69f42f
Fedorov113 <fedorov.de@gmail.com>
2020-03-17 19:40:44 +0300

    Update cat_bat.py

--- assnake_core_taxonomy/cat_bat/cat_bat.py
+++ assnake_core_taxonomy/cat_bat/cat_bat.py
@@ -11,73 +11,15 @@ rule cat_bat_contigs_new:
     params:
         wd             = '{fs_prefix}/{df}/assembly/{sample_set}/{assembler}__{assembler_version}__{params}/final_contigs__{mod}_CAT/',
         prefix         = '{fs_prefix}/{df}/assembly/{sample_set}/{assembler}__{assembler_version}__{params}/final_contigs__{mod}_CAT/out.CAT',
-    threads: 12
+    threads: 16
     conda: 'cat_bat_env.yaml'
     shell: ('''mkdir -p {params.wd}; \n
-            CAT contigs -c {input.fa} -d {cat_bat_db} -t {cat_bat_taxa} --out_prefix {params.prefix} -n {threads};''')
+            CAT contigs -c {input.fa} -d {cat_bat_db} -t {cat_bat_taxa} --index_chunks 1 --I_know_what_Im_doing --top 25 --out_prefix {params.prefix} -n {threads};''')
 
 rule add_names2:
     input:
         classification = '{fs_prefix}/{df}/assembly/{sample_set}/{assembler}__{assembler_version}__{params}/final_contigs__{mod}_CAT/out.CAT.contig2classification.txt'
     output:
-        names = '{fs_prefix}/{df}/assembly/{sample_set}/{assembler}__{assembler_version}__{params}/final_contigs__{mod}_CAT/contig2classification_names_official.txt'
+        names          = '{fs_prefix}/{df}/assembly/{sample_set}/{assembler}__{assembler_version}__{params}/final_contigs__{mod}_CAT/contig2classification_names_official.txt'
     conda: 'cat_bat_env.yaml'
     shell: ('''CAT add_names -i {input.classification} -o {output.names} -t {cat_bat_taxa} --only_official''')
-
-
-# rule cat_bat_CAT:
-#     input:
-#         exported = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins.done')
-#     output:
-#         done = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/cat_bins.done')
-#     params:
-#         bin_folder = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins/'),
-#         wd = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins_cat/'),
-#         prefix = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins_cat/out.BAT'),
-#     threads: 8
-#     conda: 'cat_bat_env.yaml'
-#     shell: ('''mkdir -p {params.wd}; \n
-#         CAT bins -b {params.bin_folder} -d {cat_bat_db} -t {cat_bat_taxa} --bin_suffix .fa --out_prefix {params.prefix} -n {threads}; \n
-#             touch {output.done}''')
-
-# rule cat_bat_contigs:
-#     input:
-#         fa     = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000.fa')
-#     output:
-#         done   = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/cat.done'),
-#         classification = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/out.CAT.contig2classification.txt')
-#     params:
-#         wd     = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/'),
-#         prefix = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/out.CAT'),
-#     threads: 12
-#     conda: 'cat_bat_env.yaml'
-#     shell: ('''mkdir -p {params.wd}; \n
-#             CAT contigs -c {input.fa} -d {cat_bat_db} -t {cat_bat_taxa} --out_prefix {params.prefix} -n {threads}; \n
-#             touch {output.done}''')
-
-# rule add_names:
-#     input:
-#         done           = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/cat_bins.done'),
-#         classification = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins_cat/out.BAT.bin2classification.txt')
-#     output:
-#         names = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins_cat/bin2classification_names_official.txt')
-#     threads: 12
-#     conda: 'cat_bat_env.yaml'
-#     shell: ('''CAT add_names -i {input.classification} -o {output.names} -t {cat_bat_taxa} --only_official''')
-
-
-# rule run_mags_on_prev_run:
-#     input: 
-#         exported = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins.done'),
-#         proteins = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}_CAT/out.CAT.predicted_proteins_clean.faa'),
-#         al       = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}_CAT/out.CAT.alignment.diamond')
-#     output: done = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/cat_bins.done')
-#     params: 
-#         bin_folder = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins/'),
-#         wd         = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins_cat/'),
-#         prefix     = os.path.join(fna_db_dir, 'assembly/mh__{params}/{df}/{samples}/final_contigs__{mod}/{collection}/all_bins_cat/out.BAT'),
-#     threads: 12
-#     conda: 'cat_bat_env.yaml'
-#     shell: 'mkdir -p {params.wd}; \n CAT bins -b {params.bin_folder} -d {cat_bat_db} -t {cat_bat_taxa} \
-#         -p {input.proteins} -a {input.al} \
-#             --bin_suffix .fa --out_prefix {params.prefix};\n touch {output.done} '
