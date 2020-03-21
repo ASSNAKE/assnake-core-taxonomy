7602d9b180a37f430e0e48730d9e3e7c620fb675
albigel <dtumandda@gmail.com>
2020-03-22 01:40:20 +0300

    change invocation

--- assnake_core_taxonomy/metaphlan2/cmd_metaphlan2.py
+++ assnake_core_taxonomy/metaphlan2/cmd_metaphlan2.py
@@ -2,38 +2,20 @@ import assnake.api.loaders
 import assnake.api.sample_set
 from tabulate import tabulate
 import click, glob
+from assnake.cli.cli_utils import sample_set_construction_options, add_options, generic_command_individual_samples, generate_result_list
 
 parameters = [p.split('/')[-1].replace('.json', '') for p in glob.glob('/data11/bio/databases/ASSNAKE/params/mp2/*.json')]
 
 @click.command('metaphlan2', short_help='Taxonomic annoatation based on marker genes')
-
-@click.option('--df','-d', help='Name of the dataset', required=True )
-@click.option('--preproc','-p', help='Preprocessing to use' )
-@click.option('--samples-to-add','-s', 
-                help='Samples from dataset to process', 
-                default='', 
-                metavar='<samples_to_add>', 
-                type=click.STRING )
-@click.option('--params', help='Parameters id to use. Available parameter sets: ' + str(parameters), required=False, default = 'def')
-
-
+@add_options(sample_set_construction_options)
+@click.option('--params', help='Parameters to use', default='def', type=click.STRING )
 @click.pass_obj
-
-def mp2_invocation(config, df, preproc, samples_to_add, params):
+def mp2_invocation(config, **kwargs):
     # # check if database initialized
     # if config.get('metaphlan2', None) is None:
     #     click.secho('Metaphlan2 not initialized!', fg='red')
     #     click.echo('run ' + click.style('assnake init metaphlan2', bg='blue') + ' and follow instructions')
     #     exit()
-
-    samples_to_add = [] if samples_to_add == '' else [c.strip() for c in samples_to_add.split(',')]
-    df = assnake.api.loaders.load_df_from_db(df)
-    ss = assnake.api.sample_set.SampleSet(df['fs_prefix'], df['df'], preproc, samples_to_add=samples_to_add)
-
-    click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), 
-        headers='keys', tablefmt='fancy_grid'))
-    res_list = ss.get_locs_for_result('mp2')
-    if config.get('requests', None) is None:
-        config['requests'] = res_list
-    else:
-        config['requests'] += res_list
+    wc_str = '{fs_prefix}/{df}/reads/{preproc}__metaphlan2_{params}/{sample}_R1.fastq.gz'
+    sample_set, sample_set_name = generic_command_individual_samples(config, **kwargs)
+    config['requests'] += generate_result_list(sample_set, wc_str, **kwargs)
